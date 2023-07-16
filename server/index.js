5874c259e9d6d91ef207cc8000554c0ff30f9a3b
nafihpp <nafihpp@gmail.com>
2023-07-16 17:16:21 +0530

    server : bug -fix - PORT -env

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
+const express = require("express");
 const app = express();
+const {logger} = require("node-logger-middleware");
+const { connectDb } = require("./config/dbConfig");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+app.use(logger);
+connectDb();
+
+//routes
+app.use("/api", Auth);
+
+app.use("*", (req, res) => {
+    res.status(404).json({
+        message: "Route does not exist",
+    });
+});
+
+app.listen(PORT,() =>{
+    console.log(`Listening to ${PORT}`)
+})
